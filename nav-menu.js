6e4cc029f4896a63d646f9241124753218ccb66c
jash-debug <jash-debug@github.com>
2022-09-08 00:07:45 +0300

    create a toggle menu for humbuger button

--- nav-menu.js
+++ nav-menu.js
@@ -1,7 +1,17 @@
-const hamburger = document.querySelector('.nav__hamburger');
-const navLink = document.querySelector('.Enter-mobile-menu-here');
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.mobile__menu');
+const blurs = document.querySelector('#main');
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('call it whatever');
-  navLink.classList.toggle('call it whatever');
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+  blurs.classList.toggle('active');
 });
+
+document.querySelectorAll('.mobile__nav__link').forEach(
+  (n) => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    menu.classList.remove('active');
+    blurs.classList.remove('active');
+  }),
+);
