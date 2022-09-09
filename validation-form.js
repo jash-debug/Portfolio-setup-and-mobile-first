f3e62c3ecef7a629fadbb3431a6cee3e9daaf940
jash-debug <jash-debug@github.com>
2022-09-09 11:58:37 +0300

    Complete form validation to check for lower case

--- validation-form.js
+++ validation-form.js
@@ -3,7 +3,18 @@ const Form = document.getElementById('form__id');
 const message = document.querySelector('.red__text');
 
 Form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const emailInput = dEmail.value;
-  const emailLowerCase = emailInput.toLowerCase();
-});
+    event.preventDefault();
+    const emailInput = Email.value;
+    const emailLowerCase = emailInput.toLowerCase();
+
+    if (emailInput === emailLowerCase) {
+        Form.submit();
+      } else {
+        message.classList.remove('display__none');
+      }
+    });
+    
+    Email.addEventListener('focus', () => {
+      message.classList.add('display__none');
+    });
+    
