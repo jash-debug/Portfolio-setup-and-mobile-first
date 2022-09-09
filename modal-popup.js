7d9ceada8d8866ada912779fb997849241769fda
jash-debug <jash-debug@github.com>
2022-09-09 09:32:19 +0300

    Fix Linter errors

--- modal-popup.js
+++ modal-popup.js
@@ -1,129 +1,124 @@
 const card = {
-    imge: ['images/Snapshoot_Portfolio.svg', 'images/multipost_stories.svg', 'images/yoga_app.svg', 'images/professional_printing.svg'],
-    imge_full: ['images/Snapshoot_Portfolio_full.svg', 'images/professional_printing_full.svg', 'images/Snapshoot_Portfolio.svg', 'images/multipost_stories.svg'],
-    title: ['Tonic', 'Multi-Post Stories', 'Tonic', 'Tonic'],
-    title_f: ['Tonic', 'Multi-Post Stories', 'Facebook 360', 'Uber navigation'],
-    path: [['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015']],
-    path_f: [['CANOPY', 'BACK END DEV', '2015'], ['FACEBOOK', 'Full Stack Dev', '2015'], ['FACEBOOK', 'Full Stack Dev', '2015'], ['Uber', 'Lead Developer', '2018']],
-    description: ['A daily selection of privately personalized reads; no accounts or sign-ups required', 'A daily selection of privately personalized reads; no accounts or sign-ups required', 'A daily selection of privately personalized reads; no accounts or sign-ups required', 'A daily selection of privately personalized reads; no accounts or sign-ups required'],
-    description_f: ['A daily selection of privately personalized reads; no accounts or sign-ups required', 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.', 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.', 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.'],
-    languages: [['html', 'css', 'javaScript'], ['html', 'css', 'javaScript'], ['html', 'css', 'javaScript'], ['html', 'css', 'javaScript']],
-    languages_f: [['html', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript']],
-}
+  imge: ['images/Snapshoot_Portfolio.svg', 'images/multipost_stories.svg', 'images/yoga_app.svg', 'images/professional_printing.svg'],
+  imge_full: ['images/Snapshoot_Portfolio_full.svg', 'images/professional_printing_full.svg', 'images/Snapshoot_Portfolio.svg', 'images/multipost_stories.svg'],
+  title: ['Tonic', 'Multi-Post Stories', 'Tonic', 'Tonic'],
+  titleF: ['Tonic', 'Multi-Post Stories', 'Facebook 360', 'Uber navigation'],
+  path: [['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015']],
+  pathF: [['CANOPY', 'BACK END DEV', '2015'], ['FACEBOOK', 'Full Stack Dev', '2015'], ['FACEBOOK', 'Full Stack Dev', '2015'], ['Uber', 'Lead Developer', '2018']],
+  description: ['A daily selection of privately personalized reads; no accounts or sign-ups required', 'A daily selection of privately personalized reads; no accounts or sign-ups required', 'A daily selection of privately personalized reads; no accounts or sign-ups required', 'A daily selection of privately personalized reads; no accounts or sign-ups required'],
+  description_f: ['A daily selection of privately personalized reads; no accounts or sign-ups required', 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.', 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.', 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.'],
+  languages: [['html', 'css', 'javaScript'], ['html', 'css', 'javaScript'], ['html', 'css', 'javaScript'], ['html', 'css', 'javaScript']],
+  languagesF: [['html', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript']],
+};
 const projects = document.getElementById('project');
 
 for (let i = 0; i < card.title.length; i += 1) {
-    const article = document.createElement('article');
-    article.className = 'project';
-    const img_ = document.createElement('img');
-    img_.src = card.imge[i];
-    img_.className = 'mobile';
-    const img_f = document.createElement('img');
-    img_f.src = card.imge_full[i];
-    console.log(i);
-    if (i % 2 === 1) {
-        img_f.className = 'full reverse';
+  const article = document.createElement('article');
+  article.className = 'project';
+  const images = document.createElement('img');
+  images.src = card.imge[i];
+  images.className = 'mobile';
+  const imagesf = document.createElement('img');
+  imagesf.src = card.imge_full[i];
+  if (i % 2 === 1) {
+    imagesf.className = 'full reverse';
+  } else {
+    imagesf.className = 'full';
+  }
+
+  const projectDiv = document.createElement('div');
+
+  const innerDiv = document.createElement('div');
+
+  const languages = document.createElement('ul');
+  languages.className = 'project__language mobile';
+
+  const languagesF = document.createElement('ul');
+  languagesF.className = 'project__language full';
+
+  const title = document.createElement('h2');
+  const textTitle = document.createTextNode(card.title[i]);
+  title.className = 'project__heading poppins mobile';
+  title.appendChild(textTitle);
+
+  const titleF = document.createElement('h2');
+  const texttitleF = document.createTextNode(card.titleF[i]);
+  titleF.className = 'project__heading poppins full';
+  titleF.appendChild(texttitleF);
+
+  const path = document.createElement('ul');
+  path.className = 'project__path mobile poppins';
+  const pathF = document.createElement('ul');
+  pathF.className = 'project__path full poppins';
+
+  const x = card.path[i];
+  const xF = card.pathF[i];
+
+  const paragraph = document.createElement('p');
+  const desc = document.createTextNode(card.description[i]);
+  paragraph.className = 'project__desc mobile poppins';
+  paragraph.appendChild(desc);
+
+  const paragraphF = document.createElement('p');
+  const descF = document.createTextNode(card.description_f[i]);
+  paragraphF.className = 'project__desc full poppins';
+  paragraphF.appendChild(descF);
+
+  for (let j = 0; j < x.length; j += 1) {
+    const li = document.createElement('li');
+    if (x[j] === x[0]) {
+      li.className = 'project__path__name poppins';
+    } else {
+      li.className = 'project__path__type poppins';
     }
-    else {
-        img_f.className = 'full';
+    li.innerText = x[j];
+    path.appendChild(li);
+  }
+
+  for (let j = 0; j < xF.length; j += 1) {
+    const li = document.createElement('li');
+    if (xF[j] === xF[0]) {
+      li.className = 'project__path__name poppins';
+    } else {
+      li.className = 'project__path__type poppins';
     }
+    li.innerText = xF[j];
+    pathF.appendChild(li);
+  }
 
-    const divCard = document.createElement('div');
+  const pLanguage = card.languages[i];
+  for (let j = 0; j < pLanguage.length; j += 1) {
+    const li = document.createElement('li');
+    li.className = 'poppins';
+    li.innerText = pLanguage[j];
+    languages.appendChild(li);
+  }
 
-    const innerDiv = document.createElement('div');
+  const pLanguageF = card.languagesF[i];
+  for (let j = 0; j < pLanguageF.length; j += 1) {
+    const li = document.createElement('li');
+    li.className = 'poppins';
+    li.innerText = pLanguageF[j];
+    languagesF.appendChild(li);
+  }
 
-    const languages = document.createElement('ul');
-    languages.className = 'project__language mobile';
+  const button = document.createElement('button');
+  button.className = 'project__button poppins';
+  button.innerText = 'See Project';
+  button.href = '#';
 
-    const languages_f = document.createElement('ul');
-    languages_f.className = 'project__language full';
-
-    const title = document.createElement('h2');
-    const textTitle = document.createTextNode(card.title[i]);
-    title.className = 'project__heading poppins mobile';
-    title.appendChild(textTitle);
-
-    const title_f = document.createElement('h2');
-    const textTitle_f = document.createTextNode(card.title_f[i]);
-    title_f.className = 'project__heading poppins full';
-    title_f.appendChild(textTitle_f);
-
-    const path = document.createElement('ul');
-    path.className = 'project__path mobile poppins';
-    const path_f = document.createElement('ul');
-    path_f.className = 'project__path full poppins';
-
-    const x = card.path[i];
-    const x_f = card.path_f[i];
-
-    const paragraph = document.createElement('p');
-    const desc = document.createTextNode(card.description[i]);
-    paragraph.className = 'project__desc mobile poppins'
-    paragraph.appendChild(desc);
-
-    const paragraph_f = document.createElement('p');
-    const desc_f = document.createTextNode(card.description_f[i]);
-    paragraph_f.className = 'project__desc full poppins'
-    paragraph_f.appendChild(desc_f);
-
-    for (let j = 0; j < x.length; j += 1) {
-        const li = document.createElement('li');
-        if (x[j] === x[0]) {
-            li.className = 'project__path__name poppins';
-        }
-        else {
-            li.className = 'project__path__type poppins';
-        }
-        li.innerText = x[j];
-        path.appendChild(li);
-    }
-
-    for (let j = 0; j < x_f.length; j += 1) {
-        const li = document.createElement('li');
-        if (x_f[j] === x_f[0]) {
-            li.className = 'project__path__name poppins';
-        }
-        else {
-            li.className = 'project__path__type poppins';
-        }
-        li.innerText = x_f[j];
-        path_f.appendChild(li);
-    }
-
-    const p_language = card.languages[i];
-    for (let j = 0; j < p_language.length; j += 1) {
-        const li = document.createElement('li');
-        li.className = 'poppins';
-        li.innerText = p_language[j];
-        languages.appendChild(li);
-    }
-
-    const p_language_f = card.languages_f[i];
-    for (let j = 0; j < p_language_f.length; j += 1) {
-        const li = document.createElement('li');
-        li.className = 'poppins';
-        li.innerText = p_language_f[j];
-        languages_f.appendChild(li);
-    }
-
-
-    const button = document.createElement('button');
-    button.className = 'project__button poppins';
-    button.innerText = 'See Project';
-    button.href = '#';
-
-    innerDiv.appendChild(button);
-    article.appendChild(img_);
-    article.appendChild(img_f);
-    divCard.appendChild(title);
-    divCard.appendChild(title_f);
-    divCard.appendChild(path);
-    divCard.appendChild(path_f);
-    divCard.appendChild(paragraph);
-    divCard.appendChild(paragraph_f);
-    divCard.appendChild(languages);
-    divCard.appendChild(languages_f);
-    divCard.appendChild(innerDiv);
-    article.appendChild(divCard);
-    projects.appendChild(article);
+  innerDiv.appendChild(button);
+  article.appendChild(images);
+  article.appendChild(imagesf);
+  projectDiv.appendChild(title);
+  projectDiv.appendChild(titleF);
+  projectDiv.appendChild(path);
+  projectDiv.appendChild(pathF);
+  projectDiv.appendChild(paragraph);
+  projectDiv.appendChild(paragraphF);
+  projectDiv.appendChild(languages);
+  projectDiv.appendChild(languagesF);
+  projectDiv.appendChild(innerDiv);
+  article.appendChild(projectDiv);
+  projects.appendChild(article);
 }
--- popup.js
+++ popup.js
@@ -1,19 +1,18 @@
 const card2 = {
-    imge: ['https://media.gettyimages.com/photos/modern-nairobi-cityscape-capital-city-of-kenya-east-africa-picture-id1191388474?s=612x612', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg'],
-    imge_full: ['https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg'],
-    title: ['Tonic', 'Multi-Post Stories', 'Tonic', 'Tonic'],
-    title_f: ['Tonic', 'Multi-Post Stories', 'Facebook 360', 'Uber navigation'],
-    path: [['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015']],
-    path_f: [['CANOPY', 'BACK END DEV', '2015'], ['FACEBOOK', 'Full Stack Dev', '2015'], ['FACEBOOK', 'Full Stack Dev', '2015'], ['Uber', 'Lead Developer', '2018']],
-    description: ['Description1', 'Description2', 'Description3', 'Description3'],
-    languages: [['html', 'css', 'javaScript'], ['html', 'css', 'javaScript'], ['html', 'css', 'javaScript'], ['html', 'css', 'javaScript']],
-    languages_f: [['html', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript']],
-}
+  imge: ['https://media.gettyimages.com/photos/modern-nairobi-cityscape-capital-city-of-kenya-east-africa-picture-id1191388474?s=612x612', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg'],
+  imge_full: ['https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg', 'https://raw.githubusercontent.com/jash-debug/Portfolio-setup-and-mobile-first/ef08c55ed224641d597ca0552bfffcfa041586f3/images/multipost_stories.svg'],
+  title: ['Tonic', 'Multi-Post Stories', 'Tonic', 'Tonic'],
+  titleF: ['Tonic', 'Multi-Post Stories', 'Facebook 360', 'Uber navigation'],
+  path: [['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015'], ['CANOPY', 'BACK END DEV', '2015']],
+  pathF: [['CANOPY', 'BACK END DEV', '2015'], ['FACEBOOK', 'Full Stack Dev', '2015'], ['FACEBOOK', 'Full Stack Dev', '2015'], ['Uber', 'Lead Developer', '2018']],
+  description: ['Description1', 'Description2', 'Description3', 'Description3'],
+  languages: [['html', 'css', 'javaScript'], ['html', 'css', 'javaScript'], ['html', 'css', 'javaScript'], ['html', 'css', 'javaScript']],
+  languages_f: [['html', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript'], ['html', 'Ruby on rails', 'css', 'javaScript']],
+};
 
 const popupWindow = document.querySelector('.popup');
 const main = document.querySelector('#main');
 const btn = document.querySelectorAll('.project__button');
-
 
 // for (z = 0; z < btn.length; z++) {
 //     btn[z].addEventListener('click', () => {
@@ -23,116 +22,109 @@ const btn = document.querySelectorAll('.project__button');
 // }
 
 btn.forEach((button, i) => {
-    button.addEventListener("click", () => {
-        popupWindow.classList.toggle('show');
-        main.style.filter = ('blur(50px)');
+  button.addEventListener('click', () => {
+    popupWindow.classList.toggle('show');
+    main.style.filter = ('blur(50px)');
 
-        const article = document.createElement('article');
-        article.className = 'project';
-        const img_ = document.createElement('img');
-        img_.src = card.imge[i];
-        img_.className = 'mobile';
-        const img_f = document.createElement('img');
-        img_f.src = card.imge_full[i];
-        img_f.className = 'full';
+    const article = document.createElement('article');
+    article.className = 'project';
+    const images = document.createElement('img');
+    images.src = card2.imge[i];
+    images.className = 'mobile';
+    const imagesf = document.createElement('img');
+    imagesf.src = card2.imge_full[i];
+    imagesf.className = 'full';
 
-        const divCard = document.createElement('div');
+    const projectDiv = document.createElement('div');
 
-        const innerDiv = document.createElement('div');
+    const innerDiv = document.createElement('div');
 
-        const languages = document.createElement('ul');
-        languages.className = 'project__language';
+    const languages = document.createElement('ul');
+    languages.className = 'project__language';
 
-        const title = document.createElement('h2');
-        const textTitle = document.createTextNode(card.title[i]);
-        title.className = 'project__heading poppins mobile';
-        title.appendChild(textTitle);
+    const title = document.createElement('h2');
+    const textTitle = document.createTextNode(card2.title[i]);
+    title.className = 'project__heading poppins mobile';
+    title.appendChild(textTitle);
 
-        const title_f = document.createElement('h2');
-        const textTitle_f = document.createTextNode(card.title_f[i]);
-        title_f.className = 'project__heading poppins full';
-        title_f.appendChild(textTitle_f);
+    const titleF = document.createElement('h2');
+    const texttitleF = document.createTextNode(card2.titleF[i]);
+    titleF.className = 'project__heading poppins full';
+    titleF.appendChild(texttitleF);
 
-        const path = document.createElement('ul');
-        path.className = 'project__path mobile poppins';
-        const path_f = document.createElement('ul');
-        path_f.className = 'project__path full poppins';
+    const path = document.createElement('ul');
+    path.className = 'project__path mobile poppins';
+    const pathF = document.createElement('ul');
+    pathF.className = 'project__path full poppins';
 
-        const x = card.path[i];
-        const x_f = card.path_f[i];
+    const x = card2.path[i];
+    const xF = card2.pathF[i];
 
+    const paragraph = document.createElement('p');
+    const desc = document.createTextNode(card2.description[i]);
+    paragraph.className = 'project__desc poppins';
+    paragraph.appendChild(desc);
 
-        const paragraph = document.createElement('p');
-        const desc = document.createTextNode(card.description[i]);
-        paragraph.className = 'project__desc poppins'
-        paragraph.appendChild(desc);
+    for (let j = 0; j < x.length; j += 1) {
+      const li = document.createElement('li');
+      if (x[j] === x[0]) {
+        li.className = 'project__path__name poppins';
+      } else {
+        li.className = 'project__path__type poppins';
+      }
+      li.innerText = x[j];
+      path.appendChild(li);
+    }
 
-        for (let j = 0; j < x.length; j += 1) {
-            const li = document.createElement('li');
-            if (x[j] === x[0]) {
-                li.className = 'project__path__name poppins';
-            }
-            else {
-                li.className = 'project__path__type poppins';
-            }
-            li.innerText = x[j];
-            path.appendChild(li);
-        }
+    for (let j = 0; j < xF.length; j += 1) {
+      const li = document.createElement('li');
+      if (xF[j] === xF[0]) {
+        li.className = 'project__path__name poppins';
+      } else {
+        li.className = 'project__path__type poppins';
+      }
+      li.innerText = xF[j];
+      pathF.appendChild(li);
+    }
 
-        for (let j = 0; j < x_f.length; j += 1) {
-            const li = document.createElement('li');
-            if (x_f[j] === x_f[0]) {
-                li.className = 'project__path__name poppins';
-            }
-            else {
-                li.className = 'project__path__type poppins';
-            }
-            li.innerText = x_f[j];
-            path_f.appendChild(li);
-        }
+    const pLanguage = card2.languages[i];
+    for (let j = 0; j < pLanguage.length; j += 1) {
+      const li = document.createElement('li');
+      li.className = 'poppins';
+      li.innerText = pLanguage[j];
+      languages.appendChild(li);
+    }
 
-        const p_language = card.languages[i];
-        for (let j = 0; j < p_language.length; j += 1) {
-            const li = document.createElement('li');
-            li.className = 'poppins';
-            li.innerText = p_language[j];
-            languages.appendChild(li);
-        }
+    const close = document.createElement('button');
+    close.className = 'close__button';
+    close.innerText = 'X';
+    close.href = '#';
 
-        const close = document.createElement('button');
-        close.className = 'close__button'
-        close.innerText = 'X';
-        close.href = '#';
+    const button = document.createElement('button');
+    button.className = 'project__button poppins';
+    button.innerText = 'See Project';
+    button.href = '#';
 
-        const button = document.createElement('button');
-        button.className = 'project__button poppins';
-        button.innerText = 'See Project';
-        button.href = '#';
-
-        innerDiv.appendChild(button);
-        divCard.appendChild(close);
-        divCard.appendChild(title);
-        divCard.appendChild(title_f);
-        divCard.appendChild(path);
-        divCard.appendChild(path_f);
-        divCard.appendChild(img_);
-        divCard.appendChild(img_f);
-        divCard.appendChild(paragraph);
-        divCard.appendChild(languages);
-        divCard.appendChild(innerDiv);
-        article.appendChild(divCard);
-        popupWindow.appendChild(article);
-    });
+    innerDiv.appendChild(button);
+    projectDiv.appendChild(close);
+    projectDiv.appendChild(title);
+    projectDiv.appendChild(titleF);
+    projectDiv.appendChild(path);
+    projectDiv.appendChild(pathF);
+    projectDiv.appendChild(images);
+    projectDiv.appendChild(imagesf);
+    projectDiv.appendChild(paragraph);
+    projectDiv.appendChild(languages);
+    projectDiv.appendChild(innerDiv);
+    article.appendChild(projectDiv);
+    popupWindow.appendChild(article);
+  });
 });
 
 document.querySelectorAll('.popup').forEach(
-    (n) => n.addEventListener('click', () => {
-        popupWindow.classList.remove('show');
-        main.removeAttribute('style');
-        
-    }),
+  (n) => n.addEventListener('click', () => {
+    window.location.reload();
+    popupWindow.classList.remove('show');
+    main.removeAttribute('style');
+  }),
 );
-
-
-
-
