a027386842b0e8e7e7153d4d9030babaa81cc86c
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-09 10:46:29 +0200

    Update validation.js file with function

---
+++ validation-form.js
@@ -0,0 +1,9 @@
+const Email = document.getElementById('email');
+const Form = document.getElementById('form__id');
+const message = document.querySelector('.red__text');
+
+Form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailInput = dEmail.value;
+  const emailLowerCase = emailInput.toLowerCase();
+});
